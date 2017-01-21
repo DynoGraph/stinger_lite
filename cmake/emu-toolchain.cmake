844fb65b4cea7173d1edc785b3b57747e8e2b99c
Eric Hein <ehein6@gatech.edu>
2017-01-21 14:30:25 -0500

    Emu toolchain fixes.

--- cmake/emu-toolchain.cmake
+++ cmake/emu-toolchain.cmake
@@ -1,5 +1,8 @@
 # Pass this file to the first invocation of cmake using -DCMAKE_TOOLCHAIN_FILE=
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER "/home/ehein6/local/bin/emu_clang_wrapper.sh")
-set(CMAKE_LINKER "/usr/local/Emu1/toolchain/binutils-gdb-install/bin/gossamer64-ld")
+# Stable
+#set(CMAKE_LINKER "/usr/local/Emu1/toolchain/binutils-gdb-install/bin/gossamer64-ld")
+# Nightly build
+set(CMAKE_LINKER "/home1/tdysart/Emu1-Nightly/llvm-cilk/binutils-gdb-install/bin/gossamer64-ld")
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
