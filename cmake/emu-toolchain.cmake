6e75b1cb6c1d2d39b3f0cb4fac049fbdbb373f91
Eric Hein <ehein6@gatech.edu>
2017-01-26 12:45:27 -0500

    Set emu linker flags in toolchain file.

--- cmake/emu-toolchain.cmake
+++ cmake/emu-toolchain.cmake
@@ -1,8 +1,11 @@
 # Pass this file to the first invocation of cmake using -DCMAKE_TOOLCHAIN_FILE=
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER "/home/ehein6/local/bin/emu_clang_wrapper.sh")
-# Stable
-#set(CMAKE_LINKER "/usr/local/Emu1/toolchain/binutils-gdb-install/bin/gossamer64-ld")
-# Nightly build
-set(CMAKE_LINKER "/home1/tdysart/Emu1-Nightly/llvm-cilk/binutils-gdb-install/bin/gossamer64-ld")
+
+set(LLVM_CILK_HOME "/home1/tdysart/Emu1-Nightly/llvm-cilk")
+set(CMAKE_LINKER "${LLVM_CILK_HOME}/binutils-gdb-install/bin/gossamer64-ld")
+set(MEMWEB_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/memoryweb/install")
+set(LIBC_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/musl/install")
+
+set(CMAKE_EXE_LINKER_FLAGS "-L${LIBC_INSTALL}/lib -L${MEMWEB_INSTALL}/lib -lmemoryweb -lmuslc")
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
