73e2b79973e49125f44269621159cd85db345e87
Eric Hein <ehein6@gatech.edu>
2017-01-31 10:39:52 -0500

    Adjust toolchain setup to build on gcc or emu.

--- cmake/emu-toolchain.cmake
+++ cmake/emu-toolchain.cmake
@@ -1,7 +1,9 @@
 # Pass this file to the first invocation of cmake using -DCMAKE_TOOLCHAIN_FILE=
-set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_NAME Emu1)
 set(CMAKE_C_COMPILER "/home/ehein6/local/bin/emu_clang_wrapper.sh")
 
 set(LLVM_CILK_HOME "/home1/tdysart/Emu1-Nightly/llvm-cilk")
+set(MEMWEB_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/memoryweb/install")
+set(LIBC_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/musl/install")
 set(CMAKE_LINKER "${LLVM_CILK_HOME}/binutils-gdb-install/bin/gossamer64-ld")
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
