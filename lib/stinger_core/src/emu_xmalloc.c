e9415a16278eb93f3cfa6397f45d66ad95270fb8
Eric Hein <ehein6@gatech.edu>
2017-02-06 13:02:37 -0500

    Reimplement emu_malloc so we can build on x86

--- lib/stinger_core/src/emu_xmalloc.c
+++ lib/stinger_core/src/emu_xmalloc.c
@@ -3,14 +3,31 @@
 */
 
 #include "emu_xmalloc.h"
+
+#if defined(__le64__)
+// Include definitions of mw_malloc functions
 #include <distributed.h>
 #include <memoryweb.h>
 
-
 // HACK Fix broken perror on emu
-#if defined(__le64__)
-  #include <stdio.h>
-  #define perror(X) fprintf(stderr, X "\n")
+#include <stdio.h>
+#define perror(X) fprintf(stderr, X "\n")
+
+// Provide reference implementation for non-emu platforms
+#else
+void * mw_malloc2d(size_t nelem, size_t sz) FNATTR_MALLOC;
+
+void *
+mw_malloc2d(size_t nelem, size_t sz)
+{
+  void ** ptrs = xcalloc(nelem, 8);
+  void * data = xcalloc(nelem, sz);
+  for (size_t i = 0; i < nelem; ++i)
+  {
+    ptrs[i] = data + i * sz;
+  }
+  return ptrs;
+}
 #endif
 
 void *
