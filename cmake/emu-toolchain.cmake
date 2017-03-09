b045ae4ea5cc0731fd038af8b8ee3372e3eecc66
Eric Hein <ehein6@gatech.edu>
2017-03-09 17:59:07 -0500

    Use relative path to compiler script.

--- cmake/emu-toolchain.cmake
+++ cmake/emu-toolchain.cmake
@@ -1,6 +1,6 @@
 # Pass this file to the first invocation of cmake using -DCMAKE_TOOLCHAIN_FILE=
 set(CMAKE_SYSTEM_NAME Emu1)
-set(CMAKE_C_COMPILER "/home/ehein6/local/bin/emu_clang_wrapper.sh")
+set(CMAKE_C_COMPILER "${CMAKE_SOURCE_DIR}/cmake/emu_clang_wrapper.sh")
 
 set(LLVM_CILK_HOME "/home1/tdysart/Emu1-Nightly/llvm-cilk")
 set(MEMWEB_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/memoryweb/install")
