43ee6767b6fd23f0970fc0135fd093a08c6346b2
Eric Hein <ehein6@gatech.edu>
2017-07-17 14:13:26 -0400

    Added unit test for emu_array

---
+++ src/emu_array_test.c
@@ -0,0 +1,30 @@
+#include <stinger_core/emu-array.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+
+// Define test functions
+#undef EMU_ARRAY
+#define EMU_ARRAY emu_blocked_array
+#include "emu_array_test.inc"
+#undef EMU_ARRAY
+
+#undef EMU_ARRAY
+#define EMU_ARRAY emu_striped_array
+#include "emu_array_test.inc"
+#undef EMU_ARRAY
+
+
+
+int main(int argc, char *argv[])
+{
+    fprintf(stderr, "Beginning emu_striped_array_test...\n");
+    emu_striped_array_test();
+    fprintf(stderr, "Beginning emu_blocked_array_test...\n");
+    emu_blocked_array_test();
+    printf("Done\n");
+    return 0;
+}
