aec71acb652dd6ba45989bb5a817e697e9a69557
Eric Hein <ehein6@gatech.edu>
2017-08-22 10:53:58 -0400

    Temporary code for exploring Emu pointer views.

--- src/emu_array_test.c
+++ src/emu_array_test.c
@@ -92,8 +92,88 @@ void emu_blocked_array_allocation_test()
 }
 
 
+replicated long r1;
+replicated long r2;
+
+
+void force_migration()
+{
+    // Set local copy to 5
+    r1 = 5;
+    printf("r1 = %lu\n", r1);
+    // Get pointer to local copy
+    long *ptr = &r1;
+    printf("ptr = "); print_emu_pointer(ptr); printf("\n");
+    // Get pointer to remote copy
+    ptr = mw_get_nth(ptr, 2);
+    printf("ptr = "); print_emu_pointer(ptr); printf("\n");
+    // Set remote copy to 3 (causes migration)
+    *ptr = 3;
+    // Print remote copy
+    printf("*ptr = %lu\n", *ptr);
+    // Print local copy
+    printf("r1 = %lu\n", r1);
+    // Get pointer to local copy
+    ptr = &r1;
+    printf("ptr = "); print_emu_pointer(ptr); printf("\n");
+}
+
+void print_pointers()
+{
+    void * ptr;
+
+    ptr = mw_mallocstripe(1024);
+    printf("mallocstripe: "); print_emu_pointer(ptr); printf("\n");
+
+    // Manual recommended way: add BYTES_PER_NODELET() to get to next pointer
+    printf("+ BYTES_PER_NODELET: "); print_emu_pointer(ptr + BYTES_PER_NODELET()); printf("\n");
+
+
+    printf("__MW_VIEW1_NODE_MASK__: %lx\n", __MW_VIEW1_NODE_MASK__ >> __MW_NODE_BITS__);
+
+
+    // Alternate way: use get_nth
+    printf("get_nth(ptr, 1): "); print_emu_pointer(mw_get_nth(ptr, 1)); printf("\n");
+    mw_free(ptr);
+
+    ptr = mw_localmalloc(1024, 0);
+    printf("localmalloc: "); print_emu_pointer(ptr); printf("\n");
+    mw_localfree(ptr);
+
+    ptr = mw_malloc1dlong(1024);
+    printf("malloc1d: "); print_emu_pointer(ptr); printf("\n");
+    mw_free(ptr);
+
+    ptr = mw_malloc2d(1024, 1024);
+    printf("malloc2d: "); print_emu_pointer(ptr); printf("\n");
+    printf("malloc2d[2]: "); print_emu_pointer(((void**)ptr)[2]); printf("\n");
+    printf("malloc2d + 2: "); print_emu_pointer(ptr + 2); printf("\n");
+    printf("malloc2d + 16: "); print_emu_pointer(ptr + 16); printf("\n");
+    mw_free(ptr);
+
+    long x;
+    ptr = &x;
+    printf("local: "); print_emu_pointer(ptr); printf("\n");
+
+    ptr = &r1;
+    printf("r1: "); print_emu_pointer(ptr); printf("\n");
+    ptr = mw_get_nth(&r1, 0);
+    printf("get_nth(r1, 0): "); print_emu_pointer(ptr); printf("\n");
+    ptr = mw_get_nth(&r1, 2);
+    printf("get_nth(r1, 2): "); print_emu_pointer(ptr); printf("\n");
+    ptr = mw_get_localto(&r1, mw_get_nth(&r2, 0));
+    printf("get_localto(r1, get_nth(r2, 0): "); print_emu_pointer(ptr); printf("\n");
+    ptr = mw_get_localto(&r1, mw_get_nth(&r2, 2));
+    printf("get_localto(r1, get_nth(r2, 2): "); print_emu_pointer(ptr); printf("\n");
+}
+
+
 int main(int argc, char *argv[])
 {
+
+    // force_migration();
+    // print_pointers();
+
     fprintf(stderr, "Beginning emu_striped_array_test...\n");
     emu_striped_array_test();
 
