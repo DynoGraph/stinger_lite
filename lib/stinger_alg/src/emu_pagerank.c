b2d440cbc8b212858d2f8f26785285ef37b56d8c
Eric Hein <ehein6@gatech.edu>
2017-09-07 15:35:23 -0400

    Moved first step to parallel spawn

--- lib/stinger_alg/src/emu_pagerank.c
+++ lib/stinger_alg/src/emu_pagerank.c
@@ -24,6 +24,62 @@ page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, d
   return page_rank(S, NV, pr, tmp_pr_in, epsilon, dampingfactor, maxiter);
 }
 
+#define RECURSIVE_CILK_SPAWN(BEGIN, END, GRAIN, FUNC, ...)         \
+do {                                                                \
+    size_t low = BEGIN;                                             \
+    size_t high = END;                                              \
+                                                                    \
+    for (;;) {                                                      \
+        /* How many elements in my range? */                        \
+        int64_t count = high - low;                                 \
+                                                                    \
+        /* Break out when my range is smaller than the grain size */\
+        if (count <= GRAIN) break;                                  \
+                                                                    \
+        /* Divide the range in half */                              \
+        /* Invariant: count >= 2 */                                 \
+        int64_t mid = low + count / 2;                              \
+                                                                    \
+        /* Spawn a thread to deal with the lower half */            \
+        cilk_spawn FUNC(low, mid, GRAIN, __VA_ARGS__);              \
+                                                                    \
+        low = mid;                                                  \
+    }                                                               \
+                                                                    \
+    /* Recursive base case: call worker function */                 \
+    FUNC ## _worker(low, high, __VA_ARGS__);                        \
+} while (0)
+
+
+static void calculate_pagerank_worker(size_t begin, size_t end, stinger_t * S, int64_t NV, double * pr, double * tmp_pr, double * pr_constant)
+{
+  double pr_constant_local = 0;
+  for (uint64_t v = begin; v < end; v++) {
+    tmp_pr[v] = 0;
+
+    if (stinger_outdegree(S, v) == 0) {
+      pr_constant_local += pr[v];
+    } else {
+      STINGER_FORALL_IN_EDGES_OF_VTX_BEGIN(S, v) {
+        int64_t outdegree = stinger_outdegree (S, STINGER_EDGE_DEST);
+        tmp_pr[v] += pr[STINGER_EDGE_DEST] / (outdegree ? outdegree : NV-1);
+      } STINGER_FORALL_IN_EDGES_OF_VTX_END();
+    }
+  }
+  // Perform partial reduction
+  // Can't use an atomic or remote here because pr_constant is a double
+  pr_constant_local += readfe((uint64_t*)&pr_constant);
+  writeef((uint64_t*)&pr_constant, pr_constant_local);
+
+}
+
+static void calculate_pagerank(size_t begin, size_t end, size_t grain, stinger_t * S, int64_t NV, double * pr, double * tmp_pr, double * pr_constant)
+{
+  RECURSIVE_CILK_SPAWN(begin, end, grain,
+     calculate_pagerank, S, NV, pr, tmp_pr, pr_constant);
+}
+
+
 // NOTE: This only works on Undirected Graphs!
 int64_t
 page_rank (stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter)
@@ -39,25 +95,16 @@ page_rank (stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double ep
 
     double pr_constant = 0.0;
 
-    OMP("omp parallel for reduction(+:pr_constant)")
-    for (uint64_t v = 0; v < NV; v++) {
-      tmp_pr[v] = 0;
+    calculate_pagerank(0, NV, 32,
+      S, NV, pr, tmp_pr, &pr_constant);
 
-      if (stinger_outdegree(S, v) == 0) {
-        pr_constant += pr[v];
-      } else {
-        STINGER_FORALL_IN_EDGES_OF_VTX_BEGIN(S, v) {
-          int64_t outdegree = stinger_outdegree (S, STINGER_EDGE_DEST);
-          tmp_pr[v] += pr[STINGER_EDGE_DEST] / (outdegree ? outdegree : NV-1);
-        } STINGER_FORALL_IN_EDGES_OF_VTX_END();
-      }
-    }
-
+    // normalize
     OMP("omp parallel for")
     for (uint64_t v = 0; v < NV; v++) {
       tmp_pr[v] = (tmp_pr[v] + pr_constant / NV) * dampingfactor + ((1-dampingfactor) / NV);
     }
 
+    // calculate_delta
     delta = 0;
     OMP("omp parallel for reduction(+:delta)")
     for (uint64_t v = 0; v < NV; v++) {
@@ -68,6 +115,7 @@ page_rank (stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double ep
     }
     //LOG_I_A("delta : %20.15e", delta);
 
+    // double_buffer
     OMP("omp parallel for")
     for (uint64_t v = 0; v < NV; v++) {
       pr[v] = tmp_pr[v];
