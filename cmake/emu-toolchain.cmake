c6f62a85dc70a970ca8941558374c7f2aecada89
Eric Hein <ehein6@gatech.edu>
2017-10-20 18:38:59 -0400

    Updated toolchain file.

--- cmake/emu-toolchain.cmake
+++ cmake/emu-toolchain.cmake
@@ -1,10 +1,17 @@
 # Pass this file to the first invocation of cmake using -DCMAKE_TOOLCHAIN_FILE=
 set(CMAKE_SYSTEM_NAME Emu1)
-set(LLVM_CILK_HOME "/usr/local/emu/toolchain")
-set(CMAKE_C_COMPILER "${CMAKE_SOURCE_DIR}/cmake/emu_clang_wrapper.sh")
+
+if (DEFINED ENV{LLVM_CILK})
+    set(LLVM_CILK_HOME $ENV{LLVM_CILK})
+else()
+    set(LLVM_CILK_HOME "/usr/local/emu")
+endif()
+
+set(CMAKE_C_COMPILER "${LLVM_CILK_HOME}/bin/emu-cc.sh")
+set(CMAKE_CXX_COMPILER "${LLVM_CILK_HOME}/bin/emu-cc.sh")
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH ${LLVM_CILK_HOME}/binutils-gdb-install/gossamer64)
+SET(CMAKE_FIND_ROOT_PATH ${LLVM_CILK_HOME}/gossamer64)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
@@ -12,12 +19,8 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(MEMWEB_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/memoryweb/install")
-set(LIBC_INSTALL "${LLVM_CILK_HOME}/memoryweb-libraries/musl/install")
-
-# In this version of the toolchain, we explicitly set the link command, and modify it to pass along the linked libaries
-set(CMAKE_LINKER "${LLVM_CILK_HOME}/binutils-gdb-install/bin/gossamer64-ld")
-set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(MEMWEB_INSTALL "${LLVM_CILK_HOME}")
+set(LIBC_INSTALL "${LLVM_CILK_HOME}")
 
 # Main CMakeLists.txt should include the following lines:
 
